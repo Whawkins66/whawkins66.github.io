f433ee3b9342a0e6198774e311eb03ce22527c05
William Hawkins <codecaesar@Wmac.local>
2017-12-24 22:24:52 -0600

    cache

--- .cache/layouts/index.js
+++
@@ -1,7 +0,0 @@
-
-  import React from "react"
-  import Component from "/Users/codecaesar/Workspace/Blog/whawkins66.github.io/src/layouts/index.js"
-  import data from "/Users/codecaesar/Workspace/Blog/whawkins66.github.io/.cache/json/layout-index.json"
-
-  export default (props) => <Component {...props} {...data} />
-  
